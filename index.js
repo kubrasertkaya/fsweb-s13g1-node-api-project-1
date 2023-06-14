efc5f2ef0ce919b8a348ddf3dd69620800da1053
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-06-14 12:42:43 +0300

    Request yapıldı.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
-
+console.log("hello world");
+server.listen(port, () => {
+  console.log("server is listening " + port);
+});
 // START YOUR SERVER HERE
